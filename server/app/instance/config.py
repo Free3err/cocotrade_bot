b5bb56260f1180baa75bfd6cd5ea3f40cde327b8
roman <roman.klyucharov@gmail.com>
2025-05-08 22:28:04 +0300

    edited some configuration data

--- server/app/instance/config.py
+++ server/app/instance/config.py
@@ -16,6 +16,7 @@ class DatabaseConfig:
 
 class LoggingConfig:
     logging.basicConfig(
+        level=logging.INFO,
         format='%(asctime)s - %(levelname)s: %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
